3f5415fdf1a3e4db0db600a0e634e679d336727e
Jeremy Kim <jrmkim50@gmail.com>
2019-05-25 14:34:46 -0400

    Initial Commit

---
+++ DriverSafety/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  DriverSafety
+//
+//  Created by Jeremy Kim on 5/25/19.
+//  Copyright © 2019 Jeremy Kim. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
